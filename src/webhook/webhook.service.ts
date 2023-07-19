a9530990e66f4b3596553df4becfec6ed9f5ff7b
lirui <lirui@createra.fun>
2023-07-19 16:42:56 +0800

    update

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -4,8 +4,8 @@ import * as shell from 'shelljs';
 function doBlogRefresh() {
   shell.cd('/home/ruios/projects/blog');
   shell.echo(`now in ${shell.pwd()}`);
-  shell.exec('git pull');
-  shell.exec('npm run build');
+  shell.exec('git pull', {async: true});
+  shell.exec('npm run build',{async: true});
   shell.echo('blog refreshed');
 }
 
