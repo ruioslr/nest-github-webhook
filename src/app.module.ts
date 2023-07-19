52b2687d08abcd8596e5e480c5f7efbaa4d74a9a
lirui <lirui@createra.fun>
2023-07-19 16:06:10 +0800

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {WebhookModule} from "./webhook/webhook.module";
+import { WebhookModule } from './webhook/webhook.module';
 
 @Module({
   imports: [WebhookModule],
--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -1,12 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import shell from 'shelljs'
+
+function doBlogRefresh() {
+  shell.cd('/home/ruios/projects/blog');
+  console.log(`now in ${shell.pwd()}`);
+  shell.exec('git pull');
+  shell.exec('npm run build');
+  console.log('blog refreshed');
+}
 
 @Injectable()
 export class WebhookService {
   getSayHelow(): string {
     return 'hello';
   }
-  postGitHook(b: any):string {
-    console.log(b)
-    return 'postGitHook'
+  postGitHook(b: any): string {
+    try {
+      if (b.ref === 'refs/heads/master' && b.repository.name === 'blog') {
+        doBlogRefresh();
+        return 'ok';
+      } else {
+        return 'not ok';
+      }
+    } catch (e) {
+      return 'error';
+    }
   }
 }
